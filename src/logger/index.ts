9ee3b57d55e655b31bd8c180e03fabc96d0b224f
Kepi <kepi@igloonet.cz>
2023-09-14 12:37:59 +0200

    Basic reproduction repo

---
+++ src/logger/index.ts
@@ -0,0 +1,3 @@
+import { createConsola as createBasicConsola } from "consola/basic";
+import { createConsola as createBrowserConsola } from "consola/browser";
+import { createConsola } from "consola/core";
